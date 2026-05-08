a3be3db84b5ff5dabe40630bb009850b321f56fe
Harshpreet <harshsingh4771@gmail.com>
2026-05-08 21:32:37 +0530

    update backend package.json

--- Front-end/src/main.tsx
+++ Front-end/src/main.tsx
@@ -2,7 +2,7 @@
   import { GoogleOAuthProvider } from "@react-oauth/google";
   import App from "./App.tsx";
   import "./styles/index.css";
-
+ 
   createRoot(document.getElementById("root")!).render(
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
